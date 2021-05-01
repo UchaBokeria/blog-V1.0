00a2217a0dc98918c49516d12261178027470c05
Unique-Designs-UI <dachi.xucishvili8@gmail.com>
2021-05-01 15:16:39 +0400

    carouse

--- app/user-side/home/home.php
+++ app/user-side/home/home.php
@@ -2,6 +2,8 @@
   <h1>Title of The Art</h1>
   <div class="Carousel-container">
     <div class="img-tmp"></div>
+    <div class="img-tmp"></div>
+    <div class="img-tmp"></div>
     <!-- Aq IMG paths ar xsnis img gray chavagde magram chaijva -->
   </div>
   <div class="text">
@@ -13,6 +15,8 @@
 <div id="exhibition">
   <h1>Title of The Art</h1>
   <div class="Carousel-container">
+    <div class="img-tmp"></div>
+    <div class="img-tmp"></div>
     <div class="img-tmp"></div>
     <!-- Aq IMG paths ar xsnis img gray chavagde magram chaijva -->
   </div>
