6b1ba136cc5a6b8eda699ff6ccb7f5fa0842d3a4
UchaBokeria <77641706+UchaBokeria@users.noreply.github.com>
2021-05-02 12:03:13 +0400

    admin side first

--- app/admin-side/index.php
+++ app/admin-side/index.php
@@ -1,2 +1,25 @@
-<?php 
-echo "admin";
+<?php ?>
+<div id="menu">
+    <i class='fas fa-moon'></i>
+    <p>Ausstellung</p>
+    <div class="glyphicon glyphicon-menu-hamburger toggle-navigation"></div>
+</div>
+
+<div id="navigation">
+    <b class="active-page">Über uns </b>
+    <i class="fa fa-close toggle-navigation"></i>
+
+    <div>
+        <p>Ausstellung</p>
+        <p>Kontakt</p>
+        <p>Blog</p>
+    </div>
+
+</div>
+
+<div class="dark-bg"></div>
+
+<div id="content">
+    <?php require_once "contact/contact.php";?>
+    <!-- AJAX FILL -->
+</div>
