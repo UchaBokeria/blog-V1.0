3d5d7f70e2f3619cdefda9290b661a5b5a181fd8
UniqueDesigns-UI <dachi.xucishvili8@gmail.com>
2021-05-08 15:42:54 +0400

    Dachi Update admin-profile pic and shit

--- app/admin-side/admin-profile/admin-profile.action.php
+++ app/admin-side/admin-profile/admin-profile.action.php
@@ -15,11 +15,12 @@
             $userid = $_REQUEST['userid'];
             $username = $_REQUEST['username']; 
             $description = $_REQUEST['description'];
-            $birthdate = $_REQUEST['birthdate'];
+            $birth_date = $_REQUEST['birth_date'];
             $nickname = $_REQUEST['nickname'];
             $email = $_REQUEST['email'];
-            var_dump($description);
-            $res = $set->updateAccount($userid,$username,$description,$birthdate,$nickname,$email);
+            $password = $_REQUEST['password'];
+            echo $birth_date;
+            $res = $set->updateAccount($userid,$username,$description,$birth_date,$nickname,$email); //passord unda gaetanos da sheicvalos
 
             $result['content'] .= "<p>Good</p>";
             
@@ -31,24 +32,27 @@
                     $result["content"] .= '
                         <div class="left-side-content">
                             <input type="hidden" name="id" id="id" value="'.$value["id"].'">
-                            <div class="profile_pic">
-                                <img src="assets/uploads/test.jpg" id="user_image">
-                            </div>
 
                             <div class="upload_image">
-                                <input type="file" id="file" name="file" />
-                                <label for="file"><img src="assets/images/upload.png"></label>
-                                <input type="submit" value="Upload" id="but_upload"> 
+                                <div class="upload_form">                                
+                                    <input type="file" id="file" name="file" />
+                                    <label for="file"><img src="assets/images/upload.png"></label>
+                                </div>
+
+                                <div class="profile_pic">
+                                    <img src="assets/uploads/test.jpg" id="user_image">
+                                </div>
                             </div>
+
                             <p>Beschreibung</p>
-                            <input type="text" name="description" value="'.$value["description"].'" id="description">
+                            <textarea type="text" name="description"  id="description">'.$value["description"].'</textarea>
                         </div>
                 
                         <div class="right-side-content">
                             <div>
                                 <label for="nickname">Benutzer</label>
-                                <input type="text" name="ickname"value="'.$value["nickname"].'" durmishxan-id="2" id="Nickname">
-                                <i class="material-icons" data-type="done">done</i>
+                                <input type="text" name="ickname"value="'.$value["nickname"].'"  id="Nickname" data-name="Nickname">
+                                <i class="material-icons" data-type="done" data-name="Nickname">done</i>
                             </div>
                             
                             <div>
@@ -59,17 +63,17 @@
                 
                             <div>
                                 <label for="Email">Email</label>
-                                <input type="text" name="Email" value="'.$value["email"].'" id="Email">
-                                <i class="material-icons" data-type="done">done</i>
+                                <input type="text" name="Email" value="'.$value["email"].'" id="Email" data-name="Email">
+                                <i class="material-icons" data-type="done" data-name="Email">done</i>
                             </div>
                 
                             <div>
                                 <label for="password">Passwort</label>
-                                <input type="password" name="password" value="" id="Password">
-                                <i class="material-icons" data-type="done">done</i>
+                                <input type="text" name="password" value="'.$value["password"].'" id="Password" data-name="Password">
+                                <i class="material-icons" data-type="done" data-name="Password">done </i>
                             </div>
                 
-                            <div>
+                            <div id="pas_repeat">
                                 <label for="repassword">Wiederholen</label>
                                 <input type="password" name="repassword" id="Repeat">
                                 <i class="material-icons" data-type="done">done</i>
@@ -77,18 +81,114 @@
                 
                             <div>
                                 <p>Geburtsdatum</p>
-                                <input type="date" name="birthdate" id="birthdate">
+                                <input type="date" name="birthdate" value="'.$value["birth_date"].'" id="birthdate">
                             </div>
-                
+                            <p  id="alert_text">Fields are not ready to update</p>
                             <div>  
                                 <button type="button" name="save" class="save" id="saveProfile">Speicher</button>
                                 <button type="button" name="discard" class="discard">Abbrechen</button>
                             </div>
                         </div>
                         ';
-        }
+            }
             break;
+        case 'upload_image':
+            if(isset($_FILES['file']['name'])){
+                $name = $_FILES['file']['name'];
+            
+                $dir = "../../../assets/uploads/".$name;
+                $imageFileType = pathinfo($dir,PATHINFO_EXTENSION);
+            
+            
+                $imgType = strtolower(pathinfo($dir,PATHINFO_EXTENSION));
+            
+                $response = 0;
+            
+                //size
+                if($_FILES["file"]["size"] > 50000){
+                    echo "File is too big";
+                    $response = 1;
+                }
+            
+                //type
+                if($imgType != "jpg" && $imgType != "png" && $imgType != "jpeg" && $imgType != "gif"){
+                    echo "This ile is not an image";
+                    $response = 1;
+                }
+            
+                //if failed
+                if($response == 1){
+                    echo "There is a problem and file is not uploaded";
+                }
+                
+                $get_image = $get->aboutAdmin(1);
+                
+                $userid = 2;
+            
+                foreach($get_image as $value){
+                    $img_var =  $value["profile_pic"]; //aq vigeb dbshi ra qvia img(tu ra tqma unda carieli araa);
+                }
+            
+                if(empty($img_var)){ //Tu DB shi profile_pic ari carieli mashin sheva ifshi da dbshic atvirtavs da foldershic chaagdebs
+                    $set->uppdateAccountPicutre($userid,$name);
+                    if(move_uploaded_file($_FILES['file']['tmp_name'],$dir)){
+                        $response = $dir;
+                    }
+                }
+                else{ // Tu db shi aris profile_pic mashin assetshi sheva mag profile picis name rasac aq washlis da magis names daarqmevs axal potos 
+                    $dir = "../../../assets/uploads/".$img_var;
+                    if(file_exists($dir)){
+                        unlink($dir);
+                        $response = $dir;
+                    }
+                    move_uploaded_file($_FILES['file']['tmp_name'],$dir);
+                    $response = $dir;
+                    
+                }
+                echo $response;
+                exit;
+            }
+            echo 0;
+            break;
+        case 'tmp_upload':
+            $name = $_FILES['file']['name'];
+        
+            $dir = "../../../assets/uploads/tmp/".$name;
+            $imageFileType = pathinfo($dir,PATHINFO_EXTENSION);
+        
+            $imgType = strtolower(pathinfo($dir,PATHINFO_EXTENSION));
+        
+            $response = 0;
+        
+            //size
+            if($_FILES["file"]["size"] > 50000){
+                echo "File is too big";
+                $response = 1;
+            }
+        
+            //type
+            if($imgType != "jpg" && $imgType != "png" && $imgType != "jpeg" && $imgType != "gif"){
+                echo "This ile is not an image";
+                $response = 1;
+            }
+        
+            //if failed
+            if($response == 1){
+                echo "There is a problem and file is not uploaded";
+            }
+            
+            
+            $userid = 2;
 
+        
+            if(move_uploaded_file($_FILES['file']['tmp_name'],$dir)){
+                $response = $dir;
+            }
+
+            echo $response;
+            exit;
+            echo 0;
+            break;
     }
 
     
